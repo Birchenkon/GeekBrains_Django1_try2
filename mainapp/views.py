fa9fd00e339a1745f0308bdac2e340efd7949457
Anastasija <birchenkon@gmail.com>
2020-05-20 21:17:29 +0300

    -add basket

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import ProductCategory, Product, Contact
+from basketapp.models import Basket
 from django.conf import settings
 from django.utils import timezone
 
@@ -19,6 +20,11 @@ def products(request, pk=None):
         {"img": "prod-3.jpg"},
         {"img": "prod-4.jpg"},
     ]
+    basket = []
+    basket_cnt = 0
+    basket_price = 0
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
 
     if pk is not None:
         if pk == 0:
@@ -28,11 +34,18 @@ def products(request, pk=None):
             category = get_object_or_404(ProductCategory, pk=pk)
             products = Product.objects.filter(category__pk=pk).order_by("price")
 
+        for bas in basket:
+            basket_cnt += bas.quantity
+            basket_price += basket_cnt * bas.product.price
+
         content = {
             "title": title,
             "links_menu": links_menu,
             "category": category,
             "products": products,
+            "basket": basket,
+            "basket_cnt": basket_cnt,
+            "basket_price": basket_price,
         }
         return render(request, "mainapp/products_list.html", content)
 
